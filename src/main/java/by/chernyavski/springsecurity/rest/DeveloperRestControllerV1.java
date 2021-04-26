862fd0ca5d9c67874e2fc695499659ae1da82ef5
VladChernyavski <chernyavskivl@gmail.com>
2021-04-26 23:47:04 +0300

    Impl PreAuthorized authorization

--- src/main/java/by/chernyavski/springsecurity/rest/DeveloperRestControllerV1.java
+++ src/main/java/by/chernyavski/springsecurity/rest/DeveloperRestControllerV1.java
@@ -1,6 +1,7 @@
 package by.chernyavski.springsecurity.rest;
 
 import by.chernyavski.springsecurity.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class DeveloperRestControllerV1 {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public Developer getById(@PathVariable Long id) {
         return DEVELOPERS.stream()
                 .filter(d -> d.getId().equals(id))
@@ -31,12 +33,14 @@ public class DeveloperRestControllerV1 {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Developer create(@RequestBody Developer developer) {
         this.DEVELOPERS.add(developer);
         return developer;
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:write')")
     public void deleteById(@PathVariable Long id) {
         this.DEVELOPERS.removeIf(d -> d.getId().equals(id));
     }
